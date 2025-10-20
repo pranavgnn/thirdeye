241bffaf4742fe34d9c4331639aeb14b80d0d071
pranavgnn <pranavgnayak@gmail.com>
2025-10-20 11:25:25 +0530

    first commit

---
+++ vision.py
@@ -0,0 +1,46 @@
+from langchain.chat_models import init_chat_model
+from pydantic import BaseModel, Field
+from typing import Optional
+
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
+
+class ImageAnalysisResult(BaseModel):
+    """Analyse a traffic violation image and extract relevant details."""
+
+    vehicle_detected: bool = Field(description="Indicates if a vehicle is detected in the image.")
+    is_violation: Optional[bool] = Field(description="Indicates if a traffic violation is present in the image, if vehicle is present.")
+    license_plate: Optional[str] = Field(description="The license plate number of the vehicle involved in the violation in all caps and no spaces, if detectable.")
+    short_description: Optional[str] = Field(description="A brief description of the traffic violation(s), if any.")
+    detailed_description: Optional[str] = Field(description="A detailed description of the traffic violation(s), if any.")
+    violations: Optional[list[str]] = Field(description="A list of specific traffic violations detected in the image, if any.")
+    confidence_score: float = Field(description="Confidence score of the entire analysis, ranging from 0 to 1.")
+
+
+model = init_chat_model("google_genai:gemini-2.5-pro")
+structured_model = model.with_structured_output(ImageAnalysisResult)
+
+def analyze_image(image_url: str):
+    message = {
+        "role": "user",
+        "content": [
+            {
+                "type": "text", 
+                "text": "Analyze the following image for traffic violations."
+            },
+            {
+                "type": "image_url",
+                "image_url": image_url,
+            },
+        ],
+    }
+
+    return structured_model.invoke([message])
+
+
+if __name__ == "__main__":
+    analysis_result = analyze_image("https://images.hindustantimes.com/auto/img/2025/03/18/1600x900/Delhi_Traffic_Violation_1713763214287_1742269490008.jpg")
+    print(analysis_result.model_dump_json(indent=2))
