51f91c5cc01ff5551c7bdec4d0931e0ac9a56d1d
pranavgnn <pranavgnayak@gmail.com>
2025-10-22 14:53:03 +0530

    changed models

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-VISION_MODEL = "google_genai:gemini-2.5-pro"
-VIOLATION_VALIDATOR_MODEL = "google_genai:gemini-2.5-flash-lite"
+VISION_MODEL = "google_genai:gemini-2.5-flash"
+VIOLATION_VALIDATOR_MODEL = "google_genai:gemini-2.0-flash-lite"
 SUMMARY_MODEL = "google_genai:gemini-2.5-flash-lite"
 GENAI_EMBEDDINGS_MODEL = "text-embedding-004"
